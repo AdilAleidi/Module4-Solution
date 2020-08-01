d93b8f1d1c8d267624d30e13db487e6a17c6c2bc
AdilAleidi <adilaleidi@gmail.com>
2020-07-31 19:13:21 -0700

    Add files via upload

---
+++ js/SpeakGoodBye.js
@@ -0,0 +1,9 @@
+(function (window) { 
+	var byeSpeaker = {};
+    var speakWord = "Good Bye";
+    function speak(name) 
+      {
+          console.log(speakWord + " " + names);
+       }
+	window.byeSpeaker = byeSpeaker;
+})(window);
---
+++ js/SpeakHello.js
@@ -0,0 +1,9 @@
+(function (window) {
+	var helloSpeaker = {};
+    var speakWord = "Hello";
+    function speak(name) 
+      {
+          console.log(speakWord + " " + names);
+      }
+    window.helloSpeaker = helloSpeaker;
+})(window); 
